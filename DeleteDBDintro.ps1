e5a13fb05af76264befdf7462918e92f4cc08d76
NeoDBD <150392916+NeoDBD@users.noreply.github.com>
2023-11-28 14:21:08 +0100

    Create DeleteDBDintro.ps1

---
+++ DeleteDBDintro.ps1
@@ -0,0 +1,4 @@
+# Enter your path to your LoadingScreenPostLogin.bk2 file in the introLoc variable
+$introLoc = "D:\SteamLibrary\steamapps\common\Dead by Daylight\DeadByDaylight\Content\Movies\AdditionalLoadingScreen\LoadingScreenPostLogin.bk2"
+
+rm $introLoc
